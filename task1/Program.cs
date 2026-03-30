a11cf6e2d86f2f337095a5c96fb4f4db82a9e8ad
ihateengineering <k.sharonova24@apec.edu.kz>
2026-03-30 15:59:39 +0500

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,38 @@
+﻿public class CinemaTicket
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter movie title:");
+        string movieTitle = Console.ReadLine();
+        Console.WriteLine("enter number of tickets:");
+        int numberOfTickets = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter price per ticket:");
+        int pricePerTicket = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter movie duration (in hours):");
+        double movieDuration = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("enter screen number:");
+        int screenNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter first letter of seat row:");
+        char seatRowFirstLetter = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine("is movie 3D?:");
+        bool is3D = Convert.ToBoolean(Console.ReadLine());
+
+        int totalTicketCost = numberOfTickets * pricePerTicket;
+        double costPerHour = pricePerTicket / movieDuration;
+
+        Console.WriteLine($"movie title: {movieTitle}");
+        Console.WriteLine($"number of tickets: {numberOfTickets}");
+        Console.WriteLine($"price per ticket: {pricePerTicket}");
+        Console.WriteLine($"movie duration: {movieDuration}");
+        Console.WriteLine($"screen number: {screenNumber}");
+        Console.WriteLine($"first letter of seat row: {seatRowFirstLetter}");
+        Console.WriteLine($"is movie 3D: {is3D}");
+        Console.WriteLine($"total ticket cost: {totalTicketCost}");
+        Console.WriteLine($"cost per hour of movie: {costPerHour}");
+
+        Console.WriteLine($"type of movieTitle: {movieTitle.GetType()}");
+        Console.WriteLine($"type of movieDuration: {movieDuration.GetType()}");
+        Console.WriteLine($"type of is3D: {is3D.GetType()}");
+    }
+}
+
